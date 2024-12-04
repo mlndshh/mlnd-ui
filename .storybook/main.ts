5ae362f5d666473587df2d0a45af3de08a726221
Milind Sunil Shah <milindsunils@umass.edu>
2024-12-04 13:17:54 +0530

    second pr

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/**/__docs__/*.stories.tsx",
+    "../src/**/__docs__/*.mdx",
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import { peerDependencies } from "./package.json";
 
-export default defineConfig({
+export default defineConfig(() => ({
   build: {
     lib: {
       entry: "./src/index.ts", // Specifies the entry point for building the library.
@@ -17,4 +18,9 @@ export default defineConfig({
     emptyOutDir: true, // Clears the output directory before building.
   },
   plugins: [dts()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
-});
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+  },
+}));
